54f055ca8bc815c1d61f7ca38b59f5c94e24b30a
delcin-raj <delcinraj@gmail.com>
2023-12-20 17:33:44 +0530

    Home page layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,57 @@
+{{-- resources/views/layouts/app.blade.php --}}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Other head content like meta tags, main stylesheet, etc. -->
+
+    <!-- Stack for additional page-specific styles -->
+    @stack('styles')
+</head>
+<body>
+    <!-- Your layout structure -->
+
+    @include('includes.header')
+
+    <!-- Your layout structure continues -->
+
+    <div class="relative">
+        <!-- Background image -->
+        <img src="{{ asset('images/home/bg_top.jpg') }}" alt="Background" class="w-full h-auto">
+
+        <!-- Overlayed bird image, positioned to center left -->
+        <img src="{{ asset('images/home/bird.png') }}" alt="Bird" class="absolute left-1/4 w-100 h-80 transform -translate-x-1/2 -translate-y-1/2 top-1/2">
+
+        <!-- Text on center right -->
+        <div class="absolute right-1/4 transform translate-x-1/2 -translate-y-1/2 top-1/2 w-1/3">
+            <h1 class="text-4xl font-bold mb-4">Premium Ad Network</h1>
+                <p class="text-lg">Digital advertising network for web and mobile channels with excellent expertise, unique capabilities, and experience in revenue maximization for publishers and advertisers.
+                </p>
+
+            <div class="flex justify-center space-x-10 p-4 shadow rounded-lg">
+                <!-- Daily Impressions Statistic -->
+                <div class="text-center">
+                    <p class="text-sm text-gray-500 font-medium">Daily Impressions</p>
+                    <p class="text-3xl text-blue-600 font-semibold">5.8B+</p>
+                    <div class="w-20 mx-auto mt-2">
+                        <!-- Include your wavy line SVG or image here -->
+                        <img src="{{ asset('images/home/blue_squiggly.png') }}" alt="Wavy Line">
+                    </div>
+                </div>
+
+                <!-- Daily Conversions Statistic -->
+                <div class="text-center">
+                    <p class="text-sm text-gray-500 font-medium">Daily Conversions</p>
+                    <p class="text-3xl text-orange-500 font-semibold">3.6M</p>
+                    <div class="w-20 mx-auto mt-2">
+                        <!-- Include your wavy line SVG or image here -->
+                        <img src="{{ asset('images/home/blue_squiggly.png') }}" alt="Wavy Line">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,44 @@
+{{-- resources/views/components/header.blade.php --}}
+@vite('resources/css/app.css')
+<header class="bg-gray-100">
+    <nav class="container mx-auto px-4 py-3 flex justify-between items-center">
+        <a href="/" class="flex items-center">
+            <img src="{{ asset('images/logo.webp') }}" alt="Logo" class="h-10"> {{-- Tailwind class for height --}}
+            {{-- Include your logo here --}}
+        </a>
+        <div class="hidden md:flex space-x-4">
+            {{-- Navigation Links --}}
+            <a href="/advertiser" class="text-gray-700 hover:text-gray-900">Advertiser</a>
+            <a href="/publisher" class="text-gray-700 hover:text-gray-900">Publisher</a>
+            <a href="/ssp" class="text-gray-700 hover:text-gray-900">SSP</a>
+            <a href="/media-kit" class="text-gray-700 hover:text-gray-900">Media Kit</a>
+            <a href="/blog" class="text-gray-700 hover:text-gray-900">Blog</a>
+            <a href="/contacts" class="text-gray-700 hover:text-gray-900">Contacts</a>
+            <a href="/faq" class="text-gray-700 hover:text-gray-900">FAQ</a>
+        </div>
+        <div class="flex items-center space-x-4">
+            <div class="relative">
+                {{-- Dropdown for language selection --}}
+                <select class="block appearance-none bg-gray-100 border border-gray-300 text-gray-700 py-2 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                    <option>EN</option>
+                    <option>FR</option>
+                    {{-- Add other language options here --}}
+                </select>
+            </div>
+            <a href="/register" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Register
+            </a>
+            <a href="/login" class="text-blue-500 hover:bg-blue-100 font-bold py-2 px-4 rounded">
+                Sign In
+            </a>
+        </div>
+        <div class="md:hidden">
+            {{-- Mobile menu button --}}
+            <button @click="open = !open" class="text-gray-500 focus:outline-none">
+                <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                    {{-- SVG for hamburger icon here --}}
+                </svg>
+            </button>
+        </div>
+    </nav>
+</header>
